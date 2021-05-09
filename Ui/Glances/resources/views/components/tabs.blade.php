cbee16dd35fb5e9e920cfd0a4a3b050257bc4535
JJ Rohrer <jj.src@rohrer.org>
2021-05-09 10:59:45 -0400

    Tabs looks like folder tabs again

--- Ui/Glances/resources/views/components/tabs.blade.php
+++ Ui/Glances/resources/views/components/tabs.blade.php
@@ -1,5 +1,5 @@
-{{--https://laracasts.com/series/blade-component-cookbook/episodes/9--}}
 @props(['active'])
+
 <div x-data="{
        activeTab: '{{$active}}',
        tabs: [],
@@ -18,6 +18,8 @@
         toggleTabs();
      }"
      class="FYIresources/views/components/tabs.blade.php">
+    {{--from Laracast
+    https://laracasts.com/series/blade-component-cookbook/episodes/9
     <div>
         <template x-for="(tab, index) in tabHeadings" :key="index">
             <button  x-text="tab"
@@ -27,7 +29,27 @@
              role="tab"
             :aria-selected="tab === activeTab"></button>
         </template>
+    </div>--}}
+    <div>
+        <template x-for="(tab, index) in tabHeadings" :key="index">
+            <button  x-text="tab"
+                     @click="activeTab = tab; toggleTabs();"
+                     class="whitespace-no-wrap
+                                        px-3 py-2
+                                        font-medium text-sm leading-5 text-gray-600 no-underline
+
+                                    cursor-pointer
+                                        hover:text-gray-700 hover:bg-gray-200 hover:border-t hover:border-r hover:rounded-t-xlg
+                                        focus:outline-none focus:text-gray-700 focus:border-gray-300"
+                     :class="{ 'border-indigo-500 border-l border-t border-r rounded-t-lg py-1': tab === activeTab, 'border-b rounded-t' : tab !== activeTab}"
+
+                     role="tab"
+                     :aria-selected="tab === activeTab"></button>
+        </template>
     </div>
+
+
+{{--Content    --}}
     <div x-ref="tabs">{!! $slot !!}</div>
 
 </div>
--- Ui/Glances/resources/views/samples/TabSamples/TabSample1__Page.blade.php
+++ Ui/Glances/resources/views/samples/TabSamples/TabSample1__Page.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-tassy-ui::tabs :active="'Second'">
+    <x-tassy-ui::tabs :active="'First'">
         <x-tassy-ui::tab name="First" :isLivewire="true">
             <livewire:tassy-ui:Sample_Tab1_Tab :tabName="'First'"/>
         </x-tassy-ui::tab>
