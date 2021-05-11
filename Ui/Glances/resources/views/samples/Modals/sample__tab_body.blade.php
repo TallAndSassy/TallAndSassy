53e5e10aa71950aa372d65c0294b917da34850b2
JJ Rohrer <jj.src@rohrer.org>
2021-05-11 16:27:38 -0400

    more examples

--- Ui/Glances/resources/views/samples/Modals/sample__tab_body.blade.php
+++ Ui/Glances/resources/views/samples/Modals/sample__tab_body.blade.php
@@ -4,10 +4,41 @@
     Here we show the usage of a simply blade-based modal, with a smidge of Alpine to make it dynamic. Importantly, this
     is not a livewire model that might talk to the database.
     <p></p>
+
     <x-tassy-ui::button
         onclick="$modals.showBladeModalNamed('sample-modal')"
         type="primary"
     >
         Click me!
     </x-tassy-ui::button>
+    <hr>
+    So, launch a modal, make a button or link, and add an on-click
+    <pre>
+        <code class="html">
+        onclick="$modals.showBladeModalNamed('sample-modal')"
+        </code>
+    </pre>
+    <pre>
+        <code class="html">
+            @php
+                $sample =<<<EOD
+                    <x-tassy-ui::modal name="sample-modal">
+                        <x-slot name="title">A Sample Modal</x-slot>
+                        <x-slot name="body">
+                            <p>Here are some details about our stuff</p>
+                        </x-slot>
+                        <x-slot name="footer">
+                            <x-tassy-ui::button
+                                type='cancel'
+                                @click="show = false">Go Away</x-tassy-ui::button>
+                        </x-slot>
+                    </x-tassy-ui::modal>
+                EOD;
+                print htmlspecialchars($sample);
+            @endphp
+
+        </code>
+    </pre>
+
+    {{--    Look at http://127.0.0.1:8002/grok/ElegantTechnologies/Grok/grok in Teamsy10--}}
 </div>
