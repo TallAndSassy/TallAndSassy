cb3e4f09bb791e51d90008d2c5833c0398a4f674
JJ Rohrer <jj.src@rohrer.org>
2021-06-18 15:47:30 -0400

    menu size tweak

--- PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-fork-leaf.blade.php
+++ PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-fork-leaf.blade.php
@@ -1,6 +1,6 @@
 @props(['suburl','label'])
 <li>
-    <a class="linkFontColor_cssClasses   linkFocus_cssClasses ml-12 pickedClasses_ifOnThisRoute "
+    <a class="linkFontColor_cssClasses   linkFocus_cssClasses ml-10 pickedClasses_ifOnThisRoute text-sm"
         {!! \TallAndSassy\PageGuide\Components\Lepage::wireSwaplinkInA($suburl) !!}>{!! $label !!}</a>
 
 </li>
