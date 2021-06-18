b4049c617fe27680c550e9e34779aeb2e130d4ed
JJ Rohrer <jj.src@rohrer.org>
2021-06-18 15:46:35 -0400

    linen background

--- PageGuide/page-guide/resources/views/components/footer/front/index.blade.php
+++ PageGuide/page-guide/resources/views/components/footer/front/index.blade.php
@@ -1,5 +1,6 @@
 
-<div class="text-center text-xs text-80 bg-gray-200">
+<div class="text-center text-xs text-80 bg-gray-200"
+     style="background-image: url('http://127.0.0.1:8000/img/bg.png'); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed;">
     <x-tassy::footer.front.content/>
 </div>
 
