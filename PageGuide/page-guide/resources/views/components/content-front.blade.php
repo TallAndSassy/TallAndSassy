615ae82c371a6f223a9b4a8110a280d0571c47a4
JJ Rohrer <jj.src@rohrer.org>
2021-06-18 15:48:55 -0400

    css breadcrumbs

--- PageGuide/page-guide/resources/views/components/content-front.blade.php
+++ PageGuide/page-guide/resources/views/components/content-front.blade.php
@@ -1,6 +1,7 @@
 <div class="
-    tassy::page-guide-components-content-front
+    vendor.tassy::page-guide-components-content-front
     flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8
+    Hint_OverridedAndRemoveJustifyCenterToStopVerticalAlignment
     ">
   {{$slot}}
 </div>
