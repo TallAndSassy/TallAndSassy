c6987ffb6dee27fd894ae74967457c86693a373a
JJ Rohrer <jj.src@rohrer.org>
2021-07-26 17:43:20 -0400

    Wrapper for getting tenant

--- Tenancy/src/TenantUtils.php
+++ Tenancy/src/TenantUtils.php
@@ -3,7 +3,27 @@ declare(strict_types=1);
 
 namespace TallAndSassy\Tenancy;
 
+use App\Models\Tenant;
+
 class TenantUtils
 {
+    static function GetTenantIdElseNull(): ?string {
+        if (! session()->has('tenant_id')) {
+            return null;
+        } else {
+            return session()->get('tenant_id');
+        }
 
+    }
+
+    static function GetTenantSlugElseNull(): ?string {
+        $tenantIdElseNull = static::GetTenantIdElseNull();
+        if (is_null($tenantIdElseNull)) {
+            return null;
+        } else {
+            $tenantId = $tenantIdElseNull;
+            $slug = Tenant::find($tenantId)->slug;
+            return $slug;
+        }
+    }
 }
