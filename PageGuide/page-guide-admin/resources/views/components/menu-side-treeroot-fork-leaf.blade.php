e330be4a93ef14dd926dd7acdd585e33ce15499f
JJ Rohrer <jj.src@rohrer.org>
2021-07-27 16:28:01 -0400

    highlight if here

--- PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-fork-leaf.blade.php
+++ PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-fork-leaf.blade.php
@@ -1,6 +1,10 @@
 @props(['suburl','label'])
 <li>
-    <a class="linkFontColor_cssClasses   linkFocus_cssClasses ml-10 pickedClasses_ifOnThisRoute text-sm"
+    @php
+        $isOnThisRoute = \TallAndSassy\PageGuide\MenuTree::IsSubUrlMeOrBelow($suburl);
+        $pickedClasses_ifOnThisRoute = $isOnThisRoute ? 'picked_classes' : '';
+    @endphp
+    <a class="linkFontColor_cssClasses   linkFocus_cssClasses ml-10 {{$pickedClasses_ifOnThisRoute}} text-sm"
         {!! \TallAndSassy\PageGuide\Components\Lepage::wireSwaplinkInA($suburl) !!}>{!! $label !!}</a>
 
 </li>
--- PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-leaf.blade.php
+++ PageGuide/page-guide-admin/resources/views/components/menu-side-treeroot-leaf.blade.php
@@ -1,6 +1,10 @@
 @props(['suburl','label'])
 <li>
-    <a class="linkFontColor_cssClasses linkFocus_cssClasses flex items-center ml-8 px-2 py-1 leading-5 rounded-md pickedClasses_ifOnThisRoute "
+    @php
+    $isOnThisRoute = \TallAndSassy\PageGuide\MenuTree::IsSubUrlMeOrBelow($suburl);
+    $pickedClasses_ifOnThisRoute = $isOnThisRoute ? 'picked_classes' : '';
+    @endphp
+    <a class="linkFontColor_cssClasses linkFocus_cssClasses flex items-center ml-8 px-2 py-1 leading-5 rounded-md {{$pickedClasses_ifOnThisRoute}} "
         {!! \TallAndSassy\PageGuide\Components\Lepage::wireSwaplinkInA($suburl) !!}>{!! $label !!}</a>
 
 </li>
