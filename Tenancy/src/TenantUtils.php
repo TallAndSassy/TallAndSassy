f340feab7bb353162126d8de4c2f1a59c7a479f5
JJ Rohrer <jj.src@rohrer.org>
2021-07-30 17:06:04 -0400

    exceptoin vs null in get tenant

--- Tenancy/src/TenantUtils.php
+++ Tenancy/src/TenantUtils.php
@@ -16,6 +16,17 @@ class TenantUtils
 
     }
 
+    /* exception if tenant id isn't there */
+    static function GetTenantId(): string {
+        $id = session()->get('tenant_id');
+        if ($id) {
+            return $id;
+        } else {
+            throw new \Exception('No tenant session set. Maybe try GetTenantSlugElseNull if you are not sure?');
+        }
+
+    }
+
     static function GetTenantSlugElseNull(): ?string {
         $tenantIdElseNull = static::GetTenantIdElseNull();
         if (is_null($tenantIdElseNull)) {
