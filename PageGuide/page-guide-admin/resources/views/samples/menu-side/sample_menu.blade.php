c8981aac9573bfb6e4129dfa16fd0223f9df6b22
JJ Rohrer <jj.src@rohrer.org>
2021-07-30 09:49:24 -0400

    sample fix

--- PageGuide/page-guide-admin/resources/views/samples/menu-side/sample_menu.blade.php
+++ PageGuide/page-guide-admin/resources/views/samples/menu-side/sample_menu.blade.php
@@ -40,7 +40,7 @@
 <x-tassy::menu-side-divider-at-root-level/>
 
 
-<x-tassy::menu-side-treeroot>
+<x-tassy::menu-side-treeroot thisVeryLocalHandleForThisMenuTree="sample_menuside">
     <x-slot name="label">
         <div class="mr-1.5">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
