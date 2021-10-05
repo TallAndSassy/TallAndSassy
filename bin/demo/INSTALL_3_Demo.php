c0cc79989237321a02b2f03946db63445cf7380f
JJ Rohrer <jj.src@rohrer.org>
2021-10-05 13:42:37 -0400

    gave demo a --DO_FORCE_REINSTALL option

--- bin/demo/INSTALL_3_Demo.php
+++ bin/demo/INSTALL_3_Demo.php
@@ -1,5 +1,17 @@
 <?php
 require_once(__DIR__.'/../_bin_utils.php');
+
+$DO_FORCE_REINSTALL = getOptionalOption(
+    optionName:'DO_FORCE_REINSTALL',
+    default:0,
+    doesValidate:fn($passedValueToBeValidated) => in_array($passedValueToBeValidated,['0','1']),
+    transformInputToInternal:fn($passedValidatedValue) => $passedValidatedValue
+);
+
+if ($DO_FORCE_REINSTALL) {
+    jcmd(cmd:'rm database/seeders/TassyTenantSeeder.php', bForceEcho: true);
+}
+
 jcmd(cmd:'php artisan migrate:fresh', bForceEcho: true);
 
 jcmd(cmd:'php artisan vendor:publish --tag="tassy-seeders"', bForceEcho: true);
