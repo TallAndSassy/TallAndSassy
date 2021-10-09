e77d4e7313a5123d70cbb05f64e445b5e43c31a1
JJ Rohrer <jj.src@rohrer.org>
2021-10-09 08:44:56 -0400

    docs in admin config [ ] Fix Modals [ ] look at admin/config and test each one (and published blades)

--- PageGuide/page-guide-admin/config/tassy/admin.php
+++ PageGuide/page-guide-admin/config/tassy/admin.php
@@ -1,13 +1,18 @@
 <?php
 
 return [
+    // --- Quick pages (look in resources/views/vendor/tassy/admin to customize
     'DoSamples' => false,
     'DoSamples_Side_Blade' => false, //// show how to do custom menu entries on the admin's side menu
     'DoAbout' => false,
     'DoConfig' => false,
     'DoSampleDashboard' => false,
-    'DoSampleLibraryMaybePremature' => false,
     'DoHelp' => false,
+
+    // ----
     'DefaultSubSlug' => 'config',
     'DoAjaxAdminPages' => true,
+
+    // --- Future 'DoSampleLibraryMaybePremature' => false,
+    
 ];
