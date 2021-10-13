a2757caaaf9fe286dfbdac2bbb176a45ce1f60c4
JJ Rohrer <jj.src@rohrer.org>
2021-10-13 19:42:09 -0400

    tip

--- PageGuide/page-guide-admin/config/tassy/admin.php
+++ PageGuide/page-guide-admin/config/tassy/admin.php
@@ -1,7 +1,8 @@
 <?php
 
+// Tip: You usually need to run  php artisan config:clear after making changes during development
 return [
-    // --- Quick pages (look in resources/views/vendor/tassy/admin to customize
+    // --- Quick pages (look in resources/views/vendor/tassy/admin to customize)
     'DoSamples' => false,
     'DoSamples_Side_Blade' => false, //// show how to do custom menu entries on the admin's side menu
     'DoAbout' => false,
