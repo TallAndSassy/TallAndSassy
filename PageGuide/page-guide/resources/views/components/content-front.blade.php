435fd9fa330f98fa2c5c7db2080583ec0c455332
JJ Rohrer <jj.src@rohrer.org>
2021-10-16 15:07:01 -0400

    stuff

--- PageGuide/page-guide/resources/views/components/content-front.blade.php
+++ PageGuide/page-guide/resources/views/components/content-front.blade.php
@@ -2,6 +2,9 @@
     vendor.tassy::page-guide-components-content-front
     flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8
     Hint_OverridedAndRemoveJustifyCenterToStopVerticalAlignment
-    ">
+    "
+
+{{--Hint_UncommentForPrettyBackgrouns	 style="background-image: url('{{asset('img/background.png')}}'); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed;"--}}
+>
   {{$slot}}
 </div>
