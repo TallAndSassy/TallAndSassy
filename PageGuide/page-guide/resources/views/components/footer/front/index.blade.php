3eb9e9ce76198daa89a26e272c300f52ce56b9d5
JJ Rohrer <jj.src@rohrer.org>
2021-10-16 15:05:24 -0400

    hints for footer background

--- PageGuide/page-guide/resources/views/components/footer/front/index.blade.php
+++ PageGuide/page-guide/resources/views/components/footer/front/index.blade.php
@@ -1,6 +1,7 @@
 
 <div class="text-center text-xs text-80 bg-gray-200"
-     style="background-image: url('http://127.0.0.1:8000/img/bg.png'); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed;">
+{{--   Hint_UncommentForPrettyBackgrouns      style="background-image: url('{{asset('img/background.png')}}'); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed;"--}}
+>
     <x-tassy::footer.front.content/>
 </div>
 
