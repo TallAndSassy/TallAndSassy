6d3067dd9bbf98e2b1c91335151a48b56142aa63
JJ Rohrer <jj.src@rohrer.org>
2021-10-16 08:30:18 -0400

    use it

--- PageGuide/page-guide/src/Commands/TassyPageGuideCommands.php
+++ PageGuide/page-guide/src/Commands/TassyPageGuideCommands.php
@@ -39,34 +39,5 @@ class TassyPageGuideCommands extends Command
 //        print "\npackage.json was updated for Tassy-Page-Guide. Please run 'npm install' and 'run run watch' -or- 'npm run prod'\n";
     }
 
-    /**
-     * Copied from vendor/laravel/jetstream/src/Console/InstallCommand.php
-     * Update the "package.json" file.
-     *
-     * @param  callable  $callback
-     * @param  bool  $dev
-     * @return void
-     */
-    protected static function updateNodePackages(callable $callback, $dev = true)
-    {
-        if (! file_exists(base_path('package.json'))) {
-            return;
-        }
 
-        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
-
-        $packages = json_decode(file_get_contents(base_path('package.json')), true);
-
-        $packages[$configurationKey] = $callback(
-            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
-            $configurationKey
-        );
-
-        ksort($packages[$configurationKey]);
-
-        file_put_contents(
-            base_path('package.json'),
-            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
-        );
-    }
 }
