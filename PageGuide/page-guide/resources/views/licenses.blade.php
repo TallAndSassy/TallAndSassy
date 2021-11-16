a7f52f7284a962a51a35058c66a4c59b77d83ef7
JJ Rohrer <jj.src@rohrer.org>
2021-11-16 06:37:44 -0500

    make about more quick-editable

--- PageGuide/page-guide/resources/views/licenses.blade.php
+++ PageGuide/page-guide/resources/views/licenses.blade.php
@@ -1,5 +1,5 @@
 <div>
     @if (App::environment(['local', 'testing']))
-        <div class="pl-8 text-gray-300">DevOnly: If you use other licenses, like Creative Commons, put that info here. Customize this by making  /resources/views/vendor/tassy/licenses.blade.php </div>
+        <div class="pl-8 text-gray-300">DevOnly: If you use other licenses, like Creative Commons, put that info here. Customize this by making  <a href="phpstorm://open?file={{base_path('resources/views/vendor/tassy/licenses.blade.php'}}"></a>/resources/views/vendor/tassy/licenses.blade.php </div>
     @endif
 </div>
--- PageGuide/page-guide/resources/views/pretty-version.blade.php
+++ PageGuide/page-guide/resources/views/pretty-version.blade.php
@@ -1,7 +1,7 @@
 <div>
     v{{config('tassy.app-versioning.PrettyVersion','0.001a')}}
-     @if (App::environment(['local', 'testing']))
-        <div class="pl-8  text-gray-300">DevOnly:Customize look and feel of this by making  /resources/views/vendor/tassy/pretty-version.blade.php </div>
-         <div class="pl-8 text-gray-300">DevOnly: change V number via config file at config('app-versioning.PrettyVersion') </div>
+    @if (App::environment(['local', 'testing']))
+        <div class="pl-8  text-gray-300">DevOnly:Customize look and feel of this by making  <a href="phpstorm://open?file={{base_path('resources/views/vendor/tassy/pretty-version.blade.php')}}">/resources/views/vendor/tassy/pretty-version.blade.php</a> </div>
+        <div class="pl-8 text-gray-300">DevOnly: change V number via config file at <a href="phpstorm://open?file={{base_path('config/tassy/app-versioning.php')}}">config('tassy.app-versioning.PrettyVersion')</a> </div>
     @endif
 </div>
